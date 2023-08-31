21d0abfd31870d3876c551a6763e78f08a2549bc
Jonathan-Lemarie <jonathan.maheux-lemarie@dci-student.org>
2023-08-31 15:36:05 +0200

    task done

---
+++ task.py
@@ -0,0 +1,9 @@
+
+Original_String = 'The Quick BroWnFoX jumps over the laZy Dog!'
+
+print(Original_String.upper())
+#print(Original_String.lower().strip())
+
+print(Original_String.replace(' jumps over the laZy Dog!','!').lower())
+
+# still searching for a command that would take the index of 'X' and remove the rest up to '!'
